c981b9d085105ad9e0e65d89e024988a337f9661
Lucas dos Santos Corte <corte1994.lc@gmail.com>
2024-02-09 10:51:55 -0300

    feat: implementing vote-on-poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,3 +1,4 @@
+import cookie from '@fastify/cookie'
 import Fastify from 'fastify'
 import { createPoll, getPoll, getPolls, voteOnPoll } from './routes'
 
@@ -5,11 +6,17 @@ const app = Fastify({
   logger: true
 })
 
+app.register(cookie, {
+  secret: 'polls-app',
+  hook: 'onRequest'
+})
+
 const routes = [ createPoll, getPoll, getPolls, voteOnPoll ]
 
 for (const route of routes) {
   app.register(route)
 }
+
 
 try {
   app.listen({ port: 3333 }).then(() => {
