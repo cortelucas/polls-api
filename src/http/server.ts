db040809c8c8608b8ba4bb5fe18eca540f485f2c
Lucas dos Santos Corte <corte1994.lc@gmail.com>
2024-02-09 08:59:31 -0300

    feat: implementing get poll by id route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,13 @@
 import Fastify from 'fastify'
 import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
 
 const app = Fastify({
   logger: true
 })
 
 app.register(createPoll)
+app.register(getPoll)
 
 try {
   app.listen({ port: 3333 }).then(() => {
